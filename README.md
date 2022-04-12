38a33edd5d0ee21c4d7d343f4a4281bbf9fc8a73
jcorille-21 <jcorille@uw.edu>
2022-04-11 22:51:46 -0700

    ooooo it is finished

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# info201_BE_loops
+# INFO 201 BE Loops Practice
+
+The purpose of this assignment is to get practice on cloning and loops!
+Note that you **don't** have to push anything. That would cause a lot of
+merging issues! You won't have to worry about merging til later on.
+Here's what you need to do instead:
+
+- `git clone` this repository onto your machine
+- Work with a partner or two (or even more!) on the problems
+- Check in with me if you need help!
+
+More specific coding instructions are available on the `loop_practice.R` file.
---
+++ loop_practice.R
@@ -0,0 +1,26 @@
+# Create a variable called "total_ten_loops" that sums up every value in a for
+# loop that runs for ten times. What would "total_ten_loops" become as a result?
+
+
+
+# Make a for loop that runs for 13 times. On every odd occurrence of the loop,
+# the loop prints out "Odds!" On every even occurrence of the loop, the loop
+# prints out "Evens!"
+# Hint: is there a specific operator that could be used to check if a number is
+# even or odd?
+
+
+
+# Make a for loop that creates a "staircase" of letters. The letters can be
+# anything, just as long as it looks like stairs one can climb!
+# Here is an example of a staircase of letters:
+# a
+# aa
+# aaa
+# aaaa
+# aaaaa
+# Hint: would you need more than one loop to create the staircase?
+# Hint2: how would you create a new line?
+# Hint3: you might need to use sep once or twice :)
+
+
